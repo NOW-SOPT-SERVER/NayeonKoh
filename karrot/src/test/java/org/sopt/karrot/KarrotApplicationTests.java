20f72f491823918780e080a6b6f40bf34d011cf0
nykoh2001 <nykoh2001@gmail.com>
2024-04-17 03:55:32 +0900

    [chore] #5 project setting

---
+++ karrot/src/test/java/org/sopt/karrot/KarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.karrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarrotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
