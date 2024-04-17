8e01f57c56f5f322856790ede6ac88efc99b60cf
nykoh2001 <nykoh2001@gmail.com>
2024-04-17 20:19:37 +0900

    [chore] #5 add not found error code

--- karrot/src/main/java/org/sopt/karrot/exception/ErrorCode.java
+++ karrot/src/main/java/org/sopt/karrot/exception/ErrorCode.java
@@ -5,6 +5,11 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
+    // NOT FOUND
+    NOT_FOUND(40400, HttpStatus.NOT_FOUND, "해당 리소스를 찾을 수 없습니다."),
+    NOT_FOUND_MEMBER(40401, HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다."),
+
+    // INTERNAL SERVER ERROR
     INTERNAL_SERVER_ERROR(50000, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
     ;
 
