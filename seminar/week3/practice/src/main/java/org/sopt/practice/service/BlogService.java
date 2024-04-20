415bde83c9a2ee493f42fae04bafde77caef991d
nykoh2001 <nykoh2001@gmail.com>
2024-04-20 16:45:24 +0900

    [chore] add annotation

--- seminar/week3/practice/src/main/java/org/sopt/practice/service/BlogService.java
+++ seminar/week3/practice/src/main/java/org/sopt/practice/service/BlogService.java
@@ -33,9 +33,11 @@ public class BlogService {
         Blog blog = findBlogById(blogId);
         blog.updateTitle(blogTitleUpdateRequest.title());
         /* 왜 save가 없어도 update가 되나 ? blogRepository.save(blog); -> entity manager 자동 save(merge) query */
+        /* save()를 사용할 수도 있는데, 이런 경우 save()에는 @Transactional 선언이 되어 있어서 메소드에는 어노테이션 안해줘도 됨 */
     }
 
-    private Blog findBlogById(final Long blogId) {
+    @Transactional(readOnly = true)
+    protected Blog findBlogById(final Long blogId) {
         return blogRepository.findById(blogId)
                 .orElseThrow(() -> new NotFoundException(ErrorMessage.BLOG_NOT_FOUND));
     }
