8575d8e6939468c96cea803b2943bbd18c6d1fd0
nykoh2001 <nykoh2001@gmail.com>
2024-04-26 17:58:26 +0900

    [feat] #7 add create method

--- assignment/week3/practice/src/main/java/org/sopt/practice/domain/Post.java
+++ assignment/week3/practice/src/main/java/org/sopt/practice/domain/Post.java
@@ -6,8 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.sopt.practice.service.dto.PostCreateRequest;
 
 @Entity
 @Getter
@@ -24,4 +26,20 @@ public class Post extends BaseTimeEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Blog blog;
+
+    @Builder
+    private Post(final Long id, final String title, final String content, final Blog blog) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.blog = blog;
+    }
+
+    public static Post create(final PostCreateRequest postCreateRequest, final Blog blog) {
+        return Post.builder()
+                .title(postCreateRequest.title())
+                .content(postCreateRequest.content())
+                .blog(blog)
+                .build();
+    }
 }
