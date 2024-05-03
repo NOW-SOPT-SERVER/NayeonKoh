2bd5a632a7ebd16c7bc21e46b8c75247647f48e8
nykoh2001 <nykoh2001@gmail.com>
2024-05-03 20:53:15 +0900

    [feat] #9 add find by code method

--- karrot/src/main/java/org/sopt/karrot/domain/area/repository/EmdAreaRepository.java
+++ karrot/src/main/java/org/sopt/karrot/domain/area/repository/EmdAreaRepository.java
@@ -1,7 +1,15 @@
 package org.sopt.karrot.domain.area.repository;
 
+import java.util.Optional;
 import org.sopt.karrot.domain.area.domain.EmdArea;
+import org.sopt.karrot.exception.CommonException;
+import org.sopt.karrot.exception.ErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmdAreaRepository extends JpaRepository<EmdArea, Long> {
+    default EmdArea findByCodeOrThrow(final Long code) {
+        return findFirstByCode(code).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_BEOPJEONGDONG));
+    }
+
+    Optional<EmdArea> findFirstByCode(final Long code);
 }
