719138dfb40c0d47643461db64c9b959624aa65b
nykoh2001 <nykoh2001@gmail.com>
2024-05-05 22:31:26 +0900

    [fix] #9 set default value

--- karrot/src/main/java/org/sopt/karrot/domain/Member.java
+++ karrot/src/main/java/org/sopt/karrot/domain/Member.java
@@ -14,12 +14,14 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.sopt.karrot.dto.request.MemberCreateDto;
 
 @Entity
 @Getter
+@DynamicInsert
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "members")
 public class Member {
@@ -30,25 +32,22 @@ public class Member {
     @Column(name = "name", nullable = false)
     private String name;
 
-    /* columnDefinition에 대해 */
-    @Column(name = "temperature", nullable = false)
-    private Double temperature;
+    @Column(nullable = false)
+    private Double temperature = 36.5;
 
     @OneToMany(mappedBy = "seller", fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private List<Item> soldItems;
 
     @Builder
-    private Member(final String name, final Double temperature, final List<Item> soldItems) {
+    private Member(final String name, final List<Item> soldItems) {
         this.name = name;
-        this.temperature = temperature;
         this.soldItems = soldItems;
     }
 
     public static Member from(final MemberCreateDto memberCreateDto) {
         return Member.builder()
                 .name(memberCreateDto.name())
-                .temperature(36.5)
                 .soldItems(new ArrayList<>())
                 .build();
     }
