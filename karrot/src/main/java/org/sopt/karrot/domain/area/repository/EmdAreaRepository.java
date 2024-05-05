073f9d0d08be9e7acb6e8ec59b517aa32ad954e3
nykoh2001 <nykoh2001@gmail.com>
2024-05-05 22:33:23 +0900

    [chore] #9 add NonNull annotation

--- karrot/src/main/java/org/sopt/karrot/domain/area/repository/EmdAreaRepository.java
+++ karrot/src/main/java/org/sopt/karrot/domain/area/repository/EmdAreaRepository.java
@@ -1,15 +1,16 @@
 package org.sopt.karrot.domain.area.repository;
 
 import java.util.Optional;
+import lombok.NonNull;
 import org.sopt.karrot.domain.area.domain.EmdArea;
 import org.sopt.karrot.exception.CommonException;
 import org.sopt.karrot.exception.ErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmdAreaRepository extends JpaRepository<EmdArea, Long> {
-    default EmdArea findByCodeOrThrow(final Long code) {
-        return findFirstByCode(code).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_BEOPJEONGDONG));
+    default EmdArea findByIdOrThrow(final Long code) {
+        return findById(code).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_BEOPJEONGDONG));
     }
 
-    Optional<EmdArea> findFirstByCode(final Long code);
+    Optional<EmdArea> findById(@NonNull final Long locationId);
 }
