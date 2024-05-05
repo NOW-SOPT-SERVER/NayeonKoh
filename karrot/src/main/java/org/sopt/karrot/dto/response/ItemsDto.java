0b9b8bf562d667b296c12c53479536aa95a6c4d8
nykoh2001 <nykoh2001@gmail.com>
2024-05-05 22:48:33 +0900

    [fix] #9 exclude useless fields

--- karrot/src/main/java/org/sopt/karrot/dto/response/ItemsDto.java
+++ karrot/src/main/java/org/sopt/karrot/dto/response/ItemsDto.java
@@ -7,39 +7,20 @@ import org.sopt.karrot.domain.area.domain.EmdArea;
 public record ItemsDto(
         Long id,
         String title,
-        String content,
-        String tradingMethod,
         String category,
         Integer price,
-        boolean priceSuggestion,
         Integer likes,
-        EmdArea location,
-        SellerDto seller
+        EmdArea location
 
 ) {
     public static List<ItemsDto> listOf(List<Item> items) {
         return items.stream().map(item -> new ItemsDto(
                 item.getId(),
                 item.getTitle(),
-                item.getContent(),
-                item.getTradingMethod().name(),
                 item.getCategory().name(),
                 item.getPrice(),
-                item.isPriceSuggestion(),
                 item.getLikes(),
-                item.getLocation(),
-                new SellerDto(
-                        item.getSeller().getId(),
-                        item.getSeller().getName(),
-                        item.getSeller().getTemperature()
-                )
+                item.getLocation()
         )).toList();
-    }
-
-    private record SellerDto(
-            Long id,
-            String name,
-            Double temperature
-    ) {
     }
 }
