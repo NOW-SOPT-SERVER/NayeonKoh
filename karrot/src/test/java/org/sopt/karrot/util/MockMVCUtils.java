dbf0f1c0171594cc09fc31065ca5461da7daa7e9
nykoh2001 <nykoh2001@gmail.com>
2024-05-28 16:32:46 +0900

    [CHORE] #12 api docs 의존성 제거

--- karrot/src/test/java/org/sopt/karrot/util/MockMVCUtils.java
+++ karrot/src/test/java/org/sopt/karrot/util/MockMVCUtils.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.sopt.karrot.exception.GlobalExceptionHandler;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
