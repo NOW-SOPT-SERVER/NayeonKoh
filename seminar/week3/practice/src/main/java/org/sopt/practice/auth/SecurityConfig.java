1f03baad8ec5708dda7bc1592c2beb44823dae9c
nykoh2001 <nykoh2001@gmail.com>
2024-05-31 15:07:46 +0900

    [CHORE] #11 fix white list

--- seminar/week3/practice/src/main/java/org/sopt/practice/auth/SecurityConfig.java
+++ seminar/week3/practice/src/main/java/org/sopt/practice/auth/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
-    private static final String[] AUTH_WHITE_LIST = {"/api/v1/member"};
+    private static final String[] AUTH_WHITE_LIST = {"/api/v1/auth/**"};
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
