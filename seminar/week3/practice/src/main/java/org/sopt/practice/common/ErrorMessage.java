d01549834f866da718ce59d06dd13a059cf7ce75
nykoh2001 <nykoh2001@gmail.com>
2024-05-31 15:06:33 +0900

    [CHORE] #11 refresh token not found

--- seminar/week3/practice/src/main/java/org/sopt/practice/common/ErrorMessage.java
+++ seminar/week3/practice/src/main/java/org/sopt/practice/common/ErrorMessage.java
@@ -6,18 +6,19 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorMessage {
     // 400 BAD REQUEST
-    LONGER_THAN_MAX_LENGTH(HttpStatus.BAD_REQUEST.value(), "longer than max length"),
+    LONGER_THAN_MAX_LENGTH(HttpStatus.BAD_REQUEST.value(), "최대 길이보다 깁니다."),
 
     // 401 UNAUTHORIZED
     JWT_UNAUTHORIZED_EXCEPTION(HttpStatus.UNAUTHORIZED.value(), "사용자의 로그인 검증을 실패했습니다."),
 
     // 404 NOT FOUND
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "member not found"),
-    BLOG_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "blog not found"),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당하는 사용자가 없습니다."),
+    BLOG_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당하는 블로그가 없습니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당하는 리프레시 토큰이 없습니다."),
     ;
 
-    private int status;
-    private String message;
+    private final int status;
+    private final String message;
 
     ErrorMessage(final int status, final String message) {
         this.status = status;
