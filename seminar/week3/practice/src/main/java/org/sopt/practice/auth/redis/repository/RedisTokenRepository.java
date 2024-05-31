884a79375721e2abd3720fba69587eff382639d0
nykoh2001 <nykoh2001@gmail.com>
2024-05-31 15:08:23 +0900

    [FEAT] #11 throw exceptions

--- seminar/week3/practice/src/main/java/org/sopt/practice/auth/redis/repository/RedisTokenRepository.java
+++ seminar/week3/practice/src/main/java/org/sopt/practice/auth/redis/repository/RedisTokenRepository.java
@@ -2,9 +2,22 @@ package org.sopt.practice.auth.redis.repository;
 
 import java.util.Optional;
 import org.sopt.practice.auth.redis.domain.Token;
+import org.sopt.practice.common.ErrorMessage;
+import org.sopt.practice.exception.NotFoundException;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RedisTokenRepository extends CrudRepository<Token, Long> {
+
+    default Token findByRefreshTokenOrElseThrow(final String refreshToken) {
+        return findByRefreshToken(refreshToken).orElseThrow(() -> new NotFoundException(ErrorMessage.REFRESH_TOKEN_NOT_FOUND));
+    }
+
+    default Token findByIdOrElseThrow(final Long id) {
+        return findById(id).orElseThrow(() -> new NotFoundException(ErrorMessage.REFRESH_TOKEN_NOT_FOUND));
+    }
+
     Optional<Token> findByRefreshToken(final String refreshToken);
     Optional<Token> findById(final Long id);
 }
